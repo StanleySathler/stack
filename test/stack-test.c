991caff049e22a69739818dc616e313d84cb4c1d
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-05-14 19:47:29 -0300

    feat(tests): cover "push", "top" and "pop"

--- test/stack-test.c
+++ test/stack-test.c
@@ -10,9 +10,51 @@ test_stack_create()
   assert_int(-1, stack.top, "Should set the correct initial top");
 }
 
+void
+test_stack_push()
+{
+  stack_t stack = stack_create(2);
+
+  stack_push(&stack, 'a');
+  assert_int(0, stack.top, "Should increase the top to 0");
+
+  stack_push(&stack, 'c');
+  assert_int(1, stack.top, "Should increase the top to 1");
+}
+
+void
+test_stack_top()
+{
+  stack_t stack = stack_create(2);
+
+  stack_push(&stack, 'a');
+  assert_char('a', stack_top(&stack), "Should return the correct top item");
+
+  stack_push(&stack, 'c');
+  assert_char('c', stack_top(&stack), "Should return the correct top item");
+}
+
+void
+test_stack_pop()
+{
+  stack_t stack = stack_create(2);
+
+  stack_push(&stack, 'a');
+  assert_char('a', stack_pop(&stack), "Should return the correct top item");
+  assert_int(-1, stack.top, "Should decrease the top to 0");
+
+  stack_push(&stack, 'c');
+  stack_push(&stack, 'a');
+  assert_int('a', stack_pop(&stack), "Should return the correct top item");
+  assert_int(0, stack.top, "Should decrease the top to 1");
+}
+
 int
 main()
 {
   test_stack_create();
+  test_stack_push();
+  test_stack_top();
+  test_stack_pop();
   return(0);
 }
